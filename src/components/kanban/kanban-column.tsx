aa6cdd846930c6c181882aeae415f994b576df22
José Andrés Sánchez Moreno <joseandressanchezmoreno@gmail.com>
2025-08-29 14:27:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/kanban/kanban-column.tsx
+++ src/components/kanban/kanban-column.tsx
@@ -26,7 +26,7 @@ const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
     if (!enabled) {
         return null;
     }
-    return <Droppable {...props}>{children}</Droppable>;
+    return <Droppable {...props} isDropDisabled={props.isDropDisabled ?? false}>{children}</Droppable>;
 };
 
 
